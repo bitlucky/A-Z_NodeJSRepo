f2a389ccc8034fc18c767ad9d1f04800612a42e4
Lokesh Tiwari <lokesh.tiwari@byjus.com>
2022-10-04 21:42:45 +0530

    Handling different routes using express

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000
 
-// app.get('/', (req,res) => {
-//     res.send('Hello World')
-// })
-
-app.use((req,res,next) => {
-    console.log('In a middleware!');
-    next();
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>The "Add Product" Page</h1>');
 })
 
-app.use((req,res,next) => {
-    console.log('In another middleware!')
-    res.send('Hello World');
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from express</h1>');
 })
 
 app.listen(PORT, () => {
