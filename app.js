743dd1ed07e939a4ce2df4972545893685a0aa2a
Lokesh Tiwari <lokesh.tiwari@byjus.com>
2022-10-04 21:11:36 +0530

    Redirecting the requests

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req,res) => {
-    console.log('==> Request', req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>')
-    res.write('<body>Hello from my nodejs Server</body>')
-    res.write('</html>');
-    res.end();
+    const url = req.url;
+    const method = req.method;
+    if(req.url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input></form></body>')
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST') {
+        fs.writeFileSync('formMessage.txt', 'Hello this is the data coming from form');
+        return res.end();
+    }
 })
 
 server.listen(3000);
